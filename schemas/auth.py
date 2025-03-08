9759b66eb6c9d490f3b4cb0cfe9feead11d6f986
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-09 00:55:05 +1000

    Схема регистрации пользователя

---
+++ schemas/auth.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import Annotated
+import re
+
+
+class RegisterUserRequest(BaseModel):
+    name: Annotated[str, Field(..., max_length=255, examples=["nagibator_rus"])]
+    email: Annotated[str, Field(..., max_length=255, examples=["email@example.com"])]
+    password: Annotated[str, Field(max_length=40, min_length=8, examples=["!Password123"])]
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if not re.match(email_regex, value):
+            raise ValueError("Invalid email format")
+        return value
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one digit")
+
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter")
+
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError("Password must contain at least one special character")
+
+        return value
