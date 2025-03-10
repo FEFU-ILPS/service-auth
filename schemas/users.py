dc8f7c1e7590d9ebed2ab7d9707f6f9f2cd5021e
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-10 23:11:02 +1000

    Схема ответа операций над пользователями

---
+++ schemas/users.py
@@ -0,0 +1,20 @@
+import re
+from typing import Annotated
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+
+class RegisteredUserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: Annotated[UUID, Field(...)]
+    name: Annotated[str, Field(max_length=255)]
+    email: Annotated[str, Field(max_length=255)]
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if not re.match(email_regex, value):
+            raise ValueError("Invalid email format")
+        return value
