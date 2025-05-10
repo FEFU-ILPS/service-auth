1719b6531c9a5051003cf11cb193b4cc76cf3b99
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-10 16:02:25 +1000

    feature: Логирование (#2)

--- database/scripts.py
+++ database/scripts.py
@@ -1,9 +1,11 @@
 import bcrypt as bc
 
 from configs import configs
+from sqlalchemy.exc import SQLAlchemyError
 
 from .engine import LocalAsyncSession
 from .models import Password, User
+from loguru import logger
 
 
 async def init_default_admin() -> None:
@@ -11,20 +13,26 @@ async def init_default_admin() -> None:
     через переменные среды.
     """
     async with LocalAsyncSession() as session:
-        default_admin = User(
-            name=configs.default.ADMIN_NAME,
-            email=configs.default.ADMIN_EMAIL,
-            is_admin=True,
-        )
-        session.add(default_admin)
-        await session.flush()
-        await session.refresh(default_admin)
+        try:
+            default_admin = User(
+                name=configs.default.ADMIN_NAME,
+                email=configs.default.ADMIN_EMAIL,
+                is_admin=True,
+            )
+            session.add(default_admin)
+            await session.flush()
+            await session.refresh(default_admin)
 
-        hash = bc.hashpw(configs.default.ADMIN_PASSWORD.encode(), bc.gensalt())
-        default_admin_password = Password(
-            user_id=default_admin.id,
-            hash=hash,
-        )
+            hash = bc.hashpw(configs.default.ADMIN_PASSWORD.encode(), bc.gensalt())
+            default_admin_password = Password(
+                user_id=default_admin.id,
+                hash=hash.decode(),
+            )
 
-        session.add(default_admin_password)
-        await session.commit()
+            session.add(default_admin_password)
+            await session.commit()
+            logger.info("Admin account created.")
+
+        except SQLAlchemyError as _:
+            await session.rollback()
+            logger.warning("Admin account already exists.")
